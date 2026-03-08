c4471f9a04236e10a342b02b128d82a6cf2e1fe4
JeffreyKotz <unpredictabledear@gmail.com>
2026-03-08 22:04:44 +0000

    merged branch to mine for the sake of cards being tied to users

--- crinkle/crinkle/urls.py
+++ crinkle/crinkle/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from home import views
+from django.urls import path, include
 from home.views import index
 
 urlpatterns = [
