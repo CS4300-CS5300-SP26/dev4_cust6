ebce4b818b50cfa0015f868dc7db2c8b4756758c
Bryce <bholland@uccs.edu>
2026-03-10 01:51:58 +0000

    Card model, history url, view

--- crinkle/home/models.py
+++ crinkle/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# User Model
+class User(models.Model):
+    pass
+
+class Card(models.Model):
+    image = models.ImageField(upload_to='cards/')
+    name = models.CharField(max_length=100, blank=True)
+    scanned_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name or 'Unknown'} ({self.scanned_at.strftime('%Y-%m-%d %H:%M')})"
+
+class GradeReport(models.Model):
+    grade = models.TextField()
--- crinkle/home/views.py
+++ crinkle/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Card
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def history(request):
+    cards = Card.objects.all().order_by('-scanned_at')
+    return render(request, 'history.html', {'cards': cards})
