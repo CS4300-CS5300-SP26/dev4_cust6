fc02228f23d3b7d3ade9d451776a493e52907692
JJ17de <joshjones0530@icloud.com>
2026-03-11 01:53:06 +0000

    Add tracking folder with models, views, forms, and urls - templates still needed

--- crinkle/crinkle/settings.py
+++ crinkle/crinkle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v6apk9l#igpq(9@%stou5&o75hmsgoiezmu+wm*-(@)l%46872
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'tracking',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- crinkle/crinkle/urls.py
+++ crinkle/crinkle/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
 from home import views
 from home.views import index
-
 urlpatterns = [
     path('', index, name='index'),
+    path('tracking/', include('tracking.urls')),
 ]
