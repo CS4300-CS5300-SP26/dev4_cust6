e9437c153d7a0bbf8a27ff5069523c924e4a2b34
JJ17de <joshjones0530@icloud.com>
2026-03-13 02:52:55 +0000

    Tracking list with card display, add form, CSRF fix

--- crinkle/crinkle/settings.py
+++ crinkle/crinkle/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://app-jjones-21.devedu.io']
