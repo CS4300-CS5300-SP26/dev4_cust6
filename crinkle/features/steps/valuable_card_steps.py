07264e9de4993be184d498cd80f6d37247689921
JeffreyKotz <unpredictabledear@gmail.com>
2026-04-03 15:43:34 +0000

    added gherkin test and scenarios for valuable cards

---
+++ crinkle/features/steps/valuable_card_steps.py
@@ -0,0 +1,39 @@
+from behave import given, when, then
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from cards.models import CardCollection, Card, GradeReport
+
+
+@given("I have a collection of cards of value {value}")
+def cards_of_value(context, value):
+    context.collection = CardCollection.objects.create(user=context.user)
+
+    # add 10 cards for the sake of testing
+    for i in range(10):
+        notes = GradeReport.objects.create(grade="Grade")
+        card = Card.objects.create(
+            user=context.user,
+            name="Card",
+            grading_notes=notes,
+            picture_path="/",
+            user_notes="",
+            estimated_value=value,
+        )
+        card.name += f"-{card.pk}"  # add card primary key as differentiator
+        card.save()
+        context.collection.cards.add(card)
+
+
+@when("I set a value threshold of {value}")
+def set_value_threshold(context, value):
+    context.collection.value_threshold = value
+    context.collection.save()
+
+
+@then('it is "{is_valuable}" that my cards are valuable')
+def view_collection(context, is_valuable):
+    context.response = context.client.get(reverse("cards:collection"))
+
+    for card in context.response.data["cards"]:
+        assert card["valuable"] == bool(is_valuable)
---
+++ crinkle/features/valuable_card.feature
@@ -0,0 +1,23 @@
+Feature: Card Tracking
+    As a card collector
+    I want to know if a card is potentially valuable enough to be worth professional grading
+    So that I can avoid wasting money submitting low-grade cards to professional services
+
+    Scenario: Cards with value above threshold are valuable
+        Given I am logged in
+        Given I have a collection of cards of value 50
+        When I set a value threshold of 40
+        Then it is "True" that my cards are valuable
+
+    Scenario: Cards with value below threshold are not valuable
+        Given I am logged in
+        Given I have a collection of cards of value 39
+        When I set a value threshold of 40
+        Then it is "True" that my cards are valuable
+
+    Scenario: Cards matching threshold are valuable
+        Given I am logged in
+        Given I have a collection of cards of value 40
+        When I set a value threshold of 40
+        Then it is "False" that my cards are valuable
+
