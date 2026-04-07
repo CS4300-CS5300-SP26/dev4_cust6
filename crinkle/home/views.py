67f2cb49081db559982c0e96b89e20b68a644328
prasadtara <79865246+prasadtara@users.noreply.github.com>
2026-04-07 12:56:33 -0600

    Update views.py

--- crinkle/home/views.py
+++ crinkle/home/views.py
@@ -4,7 +4,7 @@ from scan.views import scan_page
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'home/index.html')
 
 def history(request):
     cards = Card.objects.all().order_by('-date_scanned')
